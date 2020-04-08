533687d42e5930cbc5b152dcc25c01ba0d2720c9
fkubota <rkkubotay@gmail.com>
2020-04-08 21:29:29 +0900

    update

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+// // defoult
+// module.exports = {
+//   "transpiledependencies": [
+//     "vuetify"
+//   ]
+// }
+
+// github pages
+module.exports = {
+  publicPath: 'simple-github-pages',
+  outputDir: 'docs',
+}
